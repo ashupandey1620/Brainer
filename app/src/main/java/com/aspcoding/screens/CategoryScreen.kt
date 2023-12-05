8243a1eb76f1638a8ea9bcb0324a224623597154
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-12-06 00:11:16 +0530

    Category Item Composable

--- app/src/main/java/com/aspcoding/screens/CategoryScreen.kt
+++ app/src/main/java/com/aspcoding/screens/CategoryScreen.kt
@@ -1,9 +1,22 @@
 package com.aspcoding.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -15,6 +28,28 @@ fun CategoryScreen() {
 fun CategoryItem(category: String) {
 
 
+    Card(modifier = Modifier
+        .padding(10.dp)
+        .size(160.dp)
+        .clip(RoundedCornerShape(8.dp)),
+        border = CardDefaults.outlinedCardBorder(enabled = true),
+        elevation = CardDefaults.elevatedCardElevation(4.dp)) {
+
+        Box (modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter){
+
+            Text(text = category,
+                fontSize = 18.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(0.dp,20.dp),
+                style = MaterialTheme.typography.bodyMedium)
+
+
+        }
+
+
+
+    }
 
 
 }
