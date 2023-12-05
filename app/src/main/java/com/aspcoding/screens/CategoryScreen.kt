226959d7ff515f6ba00697025e6684a0f05cc1d2
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-12-06 00:01:57 +0530

    Screen Setup

--- app/src/main/java/com/aspcoding/screens/CategoryScreen.kt
+++ app/src/main/java/com/aspcoding/screens/CategoryScreen.kt
@@ -1,8 +1,20 @@
 package com.aspcoding.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 
 @Composable
 fun CategoryScreen() {
     
+}
+
+@Composable
+fun CategoryItem(category: String) {
+
+
+
+
 }
